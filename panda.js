0ebe09e929253eec0a2c935942a79f037442c659
skraatzgm <86081947+skraatzgm@users.noreply.github.com>
2021-06-18 00:25:37 +0300

    Add files via upload

---
+++ panda.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const isaret = require("./isaret.json");
+const { Client, MessageEmbed } = require('discord.js');
+
+var prefix = isaret.prefix
+
+client.on('ready', () => {
+	console.log(`Sunucuya girdi botunuz ${client.user.tag}!`);
+	client.user.setActivity('Bakımda - "Gökhan"', { type: 'PLAYING'})
+	.then(presence => console.log('Durum --> aktif oldu.'))
+	.catch(console.error);
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === 'ping') {
+    msg.channel.send('Pong!');
+  }
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === 'selam') {
+    msg.author.send('Aleyküm Selam Hoşgeldin! :)');
+  }
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === prefix + ' ' + 'naber') {
+    msg.channel.send('iyidir yavru');
+  }
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === 'ananı sikm') {
+		msg.delete()
+    msg.channel.send('Çok ayıp kanka');
+  }
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === 'gökhan') {
+		msg.react('😘')
+  }
+});
+
+client.on('message', msg => {
+  if(msg.content.toLowerCase() === prefix + ' ' + 'help') {
+		const kanal = new MessageEmbed()
+
+		.setTitle('Panda Blue')
+		.setDescription('Yaratıcım -> Gökhan')
+		.setAuthor('Gökhan')
+		.setColor("RANDOM")
+		.setThumbnail('https://cdn.discordapp.com/attachments/855187027499024415/855192943773876274/unknown.png')
+		.addField(':zany_face: ' , 'Arkadaşlarınızı Davet etmeyi Unutmayın!');
+		msg.channel.send(kanal);
+	}
+	});
+
+client.login('ODU1MTcwNzc1MzU3ODQ5NjAw.YMul2Q.FodoNp8vG8zZ8BxAjX5s6A1oCVA');
